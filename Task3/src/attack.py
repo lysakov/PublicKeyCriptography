af8c297257b34b6414e9338a2b76fdce2fc5f1f0
Ivan.Lysakov <ivan@lysakov.ru>
2021-05-04 17:58:49 +0300

    UI was added

--- Task3/src/attack.py
+++ Task3/src/attack.py
@@ -6,11 +6,11 @@ class Attack(object):
     def __init__(self, timeout=60):
         self._logger = logging.getLogger("El Gamal attack logger")
         self._timeout = timeout
-        #signal.signal(signal.SIGALRM, self.__timeout_handler)
+        signal.signal(signal.SIGALRM, self.__timeout_handler)
         self._attack_func = None
 
     def attack(self, keys, cipher=None):
-        #signal.setitimer(signal.ITIMER_REAL, self._timeout, 0)
+        signal.setitimer(signal.ITIMER_REAL, self._timeout, 0)
         res = self._attack_func(keys, cipher)
         
         return res
